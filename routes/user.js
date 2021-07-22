1ca8bb5c4938a5c124bef6e7f714f3fc58493b26
Antonio <a.adame59@yahoo.com>
2021-07-21 22:15:46 -0700

    add very basic email verification

--- routes/user.js
+++ routes/user.js
@@ -3,20 +3,49 @@ const routes = express.Router();
 const User = require('../models/User');
 const CryptoJS = require("crypto-js");
 
-// POST: user/
-routes.get('/', async (req, res) => {
-    try {
-        let message = Date.now().toString();
-        let user_key = CryptoJS.SHA256(message).toString().substr(0, 16);
-        
-        let user = new User({
-            user_key: user_key,
-            email: req.body.email,
-            active: 1
-        });
+const emailValidation = async (email) => {
+    if (typeof email === 'undefined' || email == null || email === '') return {
+        success: false,
+        message: "Email field is required."
+    };
 
-        const savedUser = await user.save();
-        res.json({user_key: savedUser.user_key});
+    let user = await User.findOne({ email: email, active: true }).exec();
+    console.log('user', user);
+    if (user != null) return {
+        success: false,
+        message: "Email is already in use."
+    };
+
+    return {
+        success: true,
+        message: "Success."
+    };
+}
+
+// POST: user/
+routes.post('/', async (req, res) => {
+    try {
+        const emailValidationResult = await emailValidation(req.body.email);
+
+        if (emailValidationResult.success === false) {
+            res.json({
+                message: emailValidationResult.message
+            });
+        } else {
+            let user_key = CryptoJS.SHA256(req.body.email).toString().substr(0, 16);
+            
+            let user = new User({
+                user_key: user_key,
+                email: req.body.email,
+                active: 1
+            });
+    
+            const savedUser = await user.save();
+            res.json({
+                user_key: savedUser.user_key
+            });
+        }
+
     } catch (err) {
         res.json({message: "Unexpected error. Please try again."});
     }
